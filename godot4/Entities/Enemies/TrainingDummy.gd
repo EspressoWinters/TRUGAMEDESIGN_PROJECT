af9ac1dc34e83da1615eced79187dd71264421a1
dandrucson <dagandrucson@gmail.com>
2026-04-10 13:54:25 -0700

    added a training dummy enemy and tutorial level

---
+++ godot4/Entities/Enemies/TrainingDummy.gd
@@ -0,0 +1,19 @@
+#This is the basic enemy script, this will be the most basic enemy, the enemy will try to get to the closet player and then attack them
+#I will be retrofitting a lot of the unit's script
+#keeping the stat system for the enemy, we could then play around and have different enemies with the differing stats 
+
+#this allows us to run it in the editor
+@tool
+class_name TrainingDummy
+#we are extending the unit and now we have access to all the unit's functions and variables 
+#https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_basics.html if you need a reference on inheritance 
+extends BasicEnemy 
+
+
+func find_closet_human_character():
+	return null
+
+		
+func check_and_attack_adjacent():
+	print("")
+			
